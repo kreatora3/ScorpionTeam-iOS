fa56047c517bec32ef20d105063708392df5ba8e
kreatora3 <admin@Mac-Admin.local>
2014-11-07 10:52:44 +0200

    parse setup done

--- RoomDesigner/RoomDesigner/AppDelegate.m
+++ RoomDesigner/RoomDesigner/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,9 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [Parse setApplicationId:@"z9hPNa4lDPGMfk3PvlXdCi0kJQlptll7yoKRmqBW"
+                  clientKey:@"J9h6PPgtrKYdYofRKjL4tMJFR5DzmbVRfioK1Kqh"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     return YES;
 }
 
