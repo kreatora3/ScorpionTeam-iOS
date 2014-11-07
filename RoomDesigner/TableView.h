727464d5fd79f3bc9d8ce3ac7553cf90e1cdfc4f
Umahizi <umah@abv.bg>
2014-11-07 16:03:35 +0300

    aded some folders

---
+++ RoomDesigner/TableView.h
@@ -0,0 +1,16 @@
+//
+//  TableView.h
+//  new
+//
+//  Created by Admin on 11/7/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableView : UITableView
+
+
+@property (nonatomic, strong)NSMutableArray *content;
+
+@end
---
+++ RoomDesigner/TableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.h
+//  new
+//
+//  Created by Admin on 11/7/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@property (weak, nonatomic) IBOutlet UIImageView *productImage;
+
+@end
