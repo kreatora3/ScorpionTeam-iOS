ed951aa7dc8bceb7298c8c1492149acfde865f39
kreatora3 <admin@Mac-Admin.local>
2014-11-07 10:42:56 +0200

    initial commit

---
+++ RoomDesigner/RoomDesigner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RoomDesigner
+//
+//  Created by Admin on 11/7/14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
